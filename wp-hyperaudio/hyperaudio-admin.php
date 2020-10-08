0a7fb8fdecd45b860d2bfc852c46562cd443535e
Mark Boas <markb@happyworm.com>
2020-10-08 13:00:28 +0200

    7-restyle of admin using css grid

--- wp-hyperaudio/hyperaudio-admin.php
+++ wp-hyperaudio/hyperaudio-admin.php
@@ -25,7 +25,7 @@ function hyperaudio_options_page()
 
   <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
   <style type="text/css">
-    .wrapper {
+    /*.wrapper {
       width: 980px;
       background: #ffffff;
       margin: 0 auto;
@@ -60,7 +60,7 @@ function hyperaudio_options_page()
       margin-left:10px;
       padding: 10px;
       outline: 2px dashed #2c3e50;
-      border: none;margin-top:10px;
+      border: none;
     }
 
     textarea:hover {
@@ -107,31 +107,133 @@ function hyperaudio_options_page()
       overflow:scroll;
       overflow-x:hidden;
       height: 500px;
+    }*/
+
+    .box {
+		  color: #000;
+		  font-size: 150%;
+      background-color: #e0e0e0;
+      margin: 0;
+      padding: 0;
+	  }
+
+    .holder {
+      display: grid;
+		  grid-template-rows: repeat(2, [row] auto  );
+		  background-color: #e0e0e0;
+		  color: #000;
+      display: inline-grid;
+      padding: 20px;
+    }
+
+    .instructions-convert {
+      grid-column: col / span 2;
+      grid-row: row;
+      line-height: 150%;
+      padding-right: 40px;
+      /*max-width: 40ch;*/
+	  }
+
+	  .instructions-format {
+      grid-column: col 3 / span 2;
+      grid-row: row;
+      line-height: 150%;
+	  }
+
+    .source-title {
+      grid-column: col / span 2;
+      grid-row: row 2;
+	  }
+
+	  .target-title {
+      grid-column: col 3 / span 2;
+      grid-row: row 2;
+	  }
+
+	  .source-content {
+      grid-column: col / span 2;
+      grid-row: row 3;
+	  }
+
+	  .target-content {
+      grid-column: col 3 / span 2;
+      grid-row: row 3;
+	  }
+
+    .entry-content {
+      resize: both;
+      outline: 2px solid #fff;
+      box-shadow: 0px;
+    }
+
+    .tab {
+      border: solid;
+      border-width:0px;
+      border-bottom:thick dotted #ffffff;
+      text-decoration: none;
+      padding: 4px 4px 8px 4px;
+      font-size: 16px;
+      background-color: #fff;
+    }
+
+    a.inactive {
+      border: none;
+      text-decoration:underline;
+      background-color: #f1f1f1;
+    }
+
+    a {
+      color: #2c3e50;
+    }
+
+    #transform {
+      margin-top: 20px;
+      font-size: 24px;
+      font-weight: bold;
+      /*margin-left: 310px;*/
+      width: 100%;
+      border: solid;
+      border-width:2px;
+      padding: 8px;
+    }
+
+    textarea {
+      border: 0;
+    }
+    
+    #rtranscript {
+      padding: 8px;
+      font-size: 16px;
+      line-height: 20px;
+      overflow: scroll;
+      overflow-x: hidden;
+      height: 770px;
+      background-color: #fff;
     }
 
   </style>
 </head>
 <body>
 <div id="wrapper">
-  <header id="header">
-    <h1>Hyperaudio Converter</h1>
-  </header>
+  
+  <div class="plugin-instructions">
+    <p>When defining the source of your media using the src attribute in the hyperaudio shortcode you need to use embed versions for YouTube and SoundCloud.</p>
+    <p>For example:<p>
+    <p><code>https://www.youtube.com/embed/xLcsdc823dg</code></p>
+    <p>or</p>
+    <p><code>https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/730479133&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true</code> *</p>
+    <p>* Grab the snippet of code from the SoundCloud page containing the file you're interested in, clicking on Share and then Embed.</p>
+  </div>
 
   <div class="holder">
-    <div class="instructions">
-
-      <p>When defining the source of your media using the src attribute in the hyperaudio shortcode you need to use embed versions for YouTube and SoundCloud.</p>
-      <p>For example:<p>
-      <p>https://www.youtube.com/embed/xLcsdc823dg</p>
-      <p>https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/730479133&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true *</p>
-      <p>* You can get the snippet of code by visiting the page of the SoundCloud file you're interested in, clicking on Share and then Embed.</p>
-
-      <h2>Convert formats into a hypertranscript.</h2>
-      <p>Paste your <a href="http://en.wikipedia.org/wiki/.srt#SubRip_text_file_format">SRT (subtitle) file</a>, <a href="http://speechmatics.com">Speechmatics</a> JSON or <a href="https://lowerquality.com/gentle/">Gentle</a> JSON into the pane below and press the transform button.</p>
+    
+    <div class="box instructions-convert">
+      <h2>Convert various formats into a Hypertranscript...</h2>
+      <p>Paste your <a href="http://en.wikipedia.org/wiki/.srt#SubRip_text_file_format">SRT (subtitle) file</a>, <a href="http://speechmatics.com">Speechmatics</a> JSON, <a href="https://lowerquality.com/gentle/">Gentle</a> JSON, <a href="https://cloud.google.com/speech-to-text">Google STT</a> JSON or <a href="https://trint.com">Trint</a> JSON into the pane below and press the <strong>Convert!</strong> button.</p>
       <form>
         <!--<p><input id="line-breaks" type="checkbox" name="linebreaks" value="on"> Line breaks in output?</p>-->
         <p>
-        Input Format :
+        <strong>Input Format :</strong>
         <select id="format-select">
           <option value="srt">SRT formatted captions</option>
           <option value="speechmatics">Speechmatics JSON</option>
@@ -140,32 +242,39 @@ function hyperaudio_options_page()
           <option value="other">Trint</option>
         </select>
         </p>
-        <p><input id="word-length" type="checkbox" name="wordlength" value="on"> Take word-length into account when calculating from SRT?</p>
+        <p><input id="word-length" type="checkbox" name="wordlength" value="on"> Take word-length into account when calculating word timings from SRT?</p>
       </form>
+    </div>
+
+    <div class="box source-title">
       <h3 id="srt-title" class="entry-title">Paste here ⤵</h3>
     </div>
 
-    <div class="source-content">
+    <div class="box source-content">
       <textarea id="subtitles" class="entry-content" rows="40" cols="54"></textarea>
     </div>
-  </div>
 
-  <div class="holder">
-    <div class="instructions">
-        <form class="controls">
-            <p style="margin-top:16px">Paragraph split on delay:</p>
-            <p>0 <input id="para-split" style="width:430px" type="range" value="0" min="0.0" max="10" step="0.1"> 10<br/>
-            <span style="margin-left:200px" id="current-para-split">0</span> seconds</p>
-            <p><input id="para-punctuation" type="checkbox" name="wordlength" value="on"> Only split paras on text finishing with punctuation. (. ! ?)</p>
-        </form>
-        <p><a id="transform" href="#">Transform <img class="transform-spinner" style="display:none" src="/pad/images/ajax-loader-ffffff-on-808080.gif"></a></p>
-        <h3 id="script-title" class="entry-title">hypertranscript appears here ⤵</h3>
-        <a id="markup-view" class="tab" href="#">Markup View</a> <a id="rendered-view" class="tab inactive" href="#">Rendered View</a>
+    <div class="box target-title">
+      <h3 id="script-title" class="entry-title">hypertranscript appears here ⤵</h3>
+      <a id="markup-view" class="tab" href="#">Markup View</a> <a id="rendered-view" class="tab inactive" href="#">Rendered View</a>
     </div>
-    <div class="target-content">
+
+    <div class="box instructions-format">
+      <h2>And format.</h2>
+      <form class="controls">
+          <p>Paragraph split on delay:</p>
+          <p>0 <input id="para-split" style="width:430px" type="range" value="0" min="0.0" max="10" step="0.1"> 10<br/>
+          <span style="margin-left:200px" id="current-para-split">0</span> seconds</p>
+          <p><input id="para-punctuation" type="checkbox" name="wordlength" value="on"> Only split paras on text finishing with punctuation. (. ! ?)</p>
+      </form>
+      <p><button id="transform">Convert!</button> <img class="transform-spinner" style="display:none" src="/pad/images/ajax-loader-ffffff-on-808080.gif"></p>
+    </div>
+
+    <div class="box target-content">
       <textarea id="htranscript" class="entry-content" rows="40" cols="54"></textarea>
       <div style="display:none" id="rtranscript"></div>
     </div>
+
   </div>
 
   <div class="footer">
--- wp-hyperaudio/hyperaudio.php
+++ wp-hyperaudio/hyperaudio.php
@@ -19,7 +19,18 @@ function hyperaudio_shortcode_handler($atts, $transcript, $tag)
   $o = '';
   $src = '';
 
+  // defaults
+  $width = '100%';
+  $height = '600px';
+  //$fontfamily = '"Palatino Linotype", "Book Antiqua", Palatino, serif';
+  $fontfamily = NULL;
+  $transcriptid = NULL;
+
   if (isset($atts['src'])) $src = esc_html__($atts['src']);
+  if (isset($atts['width'])) $width = $atts['width'];
+  if (isset($atts['height'])) $height = $atts['height'];
+  if (isset($atts['font-family'])) $fontfamily = $atts['font-family'];
+  if (isset($atts['id'])) $transcriptid = $atts['id'];
 
   $transcript = preg_replace( "/\r|\n/", "", $transcript);
 
@@ -29,25 +40,13 @@ function hyperaudio_shortcode_handler($atts, $transcript, $tag)
   $globalid++;
   $id = $globalid;
 
+  if (is_null($transcriptid)) {
+    $transcriptid = "hypertranscript".$id;
+  }
+  
   $o .='<style>
-
-  .hyperaudio-transcript .strikethrough {
-    text-decoration: line-through;
-  }
   
-  .hyperaudio-transcript .annotation, .hyperaudio-transcript .parannotation{
-    opacity: 0.7;
-  }
-  
-  .hyperaudio-transcript .highlight {
-    background-color: yellow !important;
-  }
-  
-  .hyperaudio-transcript .highlight.active {
-    background-color: lightGreen !important;
-  }
-  
-  .hyperaudio-transcript  header {
+  .hyperaudio-transcript header {
     font-size: 200%;
   }
   
@@ -56,11 +55,15 @@ function hyperaudio_shortcode_handler($atts, $transcript, $tag)
   }
   
   .hyperaudio-transcript .read {
-    color: #000 !important;
+    color: #000;
   }
   
   .hyperaudio-transcript .unread {
-    color: #999 !important;
+    color: #999;
+  }
+
+  .hyperaudio-transcript .share-match {
+    background-color: #66ffad;
   }
   
   .hyperaudio-transcript sub:before {
@@ -79,9 +82,7 @@ function hyperaudio_shortcode_handler($atts, $transcript, $tag)
     cursor: pointer;
   }
   
-  
-  body {
-    font-family: "Palatino Linotype", "Book Antiqua", Palatino, serif;
+  .hyperaudio-transcript {
     line-height: 1.5;
   }
   
@@ -91,11 +92,16 @@ function hyperaudio_shortcode_handler($atts, $transcript, $tag)
   
   .hyperaudio-transcript a {
     text-decoration:none;
+  }';
+
+  if (!is_null($fontfamily)) {
+    $o .=' .hyperaudio-transcript {
+      font-family: '.$fontfamily.';
+    }';
   }
 
-  </style>';
-
-  $o .='<style>.share-this-popover{max-width:8em;pointer-events:none;-webkit-filter:drop-shadow(0 1px 3px rgba(0,0,0,.35));filter:drop-shadow(0 1px 3px rgba(0,0,0,.35));-webkit-animation:a .3s;animation:a .3s}.share-this-popover:before{content:"";position:absolute;bottom:100%;left:50%;width:0;height:0;margin:.25em -.5em;border-width:.5em .5em 0;border-style:solid;border-color:#333 transparent}.share-this-popover>ul{pointer-events:auto;list-style:none;padding:0;margin:-.75em 0 0;white-space:nowrap;background:#333;color:#fff;border-radius:.25em;position:absolute;left:50%;-webkit-transform:translate(-50%,-100%);-ms-transform:translate(-50%,-100%);transform:translate(-50%,-100%)}.share-this-popover>ul>li{display:inline-block;width:2em;height:2em;line-height:2em;text-align:center}.share-this-popover>ul>li>a{display:inline-block;width:100%;height:100%;color:inherit;box-sizing:border-box;padding:.35em}.share-this-popover>ul>li>a:focus,.share-this-popover>ul>li>a:hover{background:hsla(0,0%,100%,.25)}@media (pointer:coarse){.share-this-popover{font-size:150%}.share-this-popover:before{bottom:auto;top:100%;border-width:0 .5em .5em;margin-top:0}.share-this-popover>ul{top:100%;transform:translateX(-50%);margin:.5em 0 0}}@media (max-width:575px){.share-this-popover{left:0!important;right:0!important;width:auto!important;max-width:none}.share-this-popover:before{bottom:auto;top:100%;border-width:0 .5em .5em;margin-top:0}.share-this-popover>ul{top:100%;transform:translateX(-50%);margin:.5em 0 0;left:0;width:100%;transform:none;border-radius:0;text-align:center}}@-webkit-keyframes a{0%{-webkit-transform:translateY(-3em);opacity:0}80%{-webkit-transform:translateY(.5em);opacity:1}to{-webkit-transform:translateY(0)}}@keyframes a{0%{transform:translateY(-3em);opacity:0}80%{transform:translateY(.5em);opacity:1}to{transform:translateY(0)}}</style>';
+  $o .=' 
+  .share-this-popover{max-width:8em;pointer-events:none;-webkit-filter:drop-shadow(0 1px 3px rgba(0,0,0,.35));filter:drop-shadow(0 1px 3px rgba(0,0,0,.35));-webkit-animation:a .3s;animation:a .3s}.share-this-popover:before{content:"";position:absolute;bottom:100%;left:50%;width:0;height:0;margin:.25em -.5em;border-width:.5em .5em 0;border-style:solid;border-color:#333 transparent}.share-this-popover>ul{pointer-events:auto;list-style:none;padding:0;margin:-.75em 0 0;white-space:nowrap;background:#333;color:#fff;border-radius:.25em;position:absolute;left:50%;-webkit-transform:translate(-50%,-100%);-ms-transform:translate(-50%,-100%);transform:translate(-50%,-100%)}.share-this-popover>ul>li{display:inline-block;width:2em;height:2em;line-height:2em;text-align:center}.share-this-popover>ul>li>a{display:inline-block;width:100%;height:100%;color:inherit;box-sizing:border-box;padding:.35em}.share-this-popover>ul>li>a:focus,.share-this-popover>ul>li>a:hover{background:hsla(0,0%,100%,.25)}@media (pointer:coarse){.share-this-popover{font-size:150%}.share-this-popover:before{bottom:auto;top:100%;border-width:0 .5em .5em;margin-top:0}.share-this-popover>ul{top:100%;transform:translateX(-50%);margin:.5em 0 0}}@media (max-width:575px){.share-this-popover{left:0!important;right:0!important;width:auto!important;max-width:none}.share-this-popover:before{bottom:auto;top:100%;border-width:0 .5em .5em;margin-top:0}.share-this-popover>ul{top:100%;transform:translateX(-50%);margin:.5em 0 0;left:0;width:100%;transform:none;border-radius:0;text-align:center}}@-webkit-keyframes a{0%{-webkit-transform:translateY(-3em);opacity:0}80%{-webkit-transform:translateY(.5em);opacity:1}to{-webkit-transform:translateY(0)}}@keyframes a{0%{transform:translateY(-3em);opacity:0}80%{transform:translateY(.5em);opacity:1}to{transform:translateY(0)}}</style>';
 
   $o .='<!--<p>
     <form id="searchForm" style="float:right">
@@ -105,20 +111,20 @@ function hyperaudio_shortcode_handler($atts, $transcript, $tag)
   </p>-->';
   
   if (strpos(strtolower($src), 'youtube.com') !== false || strpos(strtolower($src), 'youtu.be') !== false) {
-    $o .= '<iframe id="hyperplayer'.$id.'" class="hyperaudio-player" data-player-type="youtube" width="400" height="300" frameborder="no" allow="autoplay" src="'.$src.'?enablejsapi=1"></iframe>';
+    $o .= '<iframe id="hyperplayer'.$id.'" class="hyperaudio-player" data-player-type="youtube" width="'.$width.'" frameborder="no" allow="autoplay" src="'.$src.'?enablejsapi=1"></iframe>';
   } elseif (strpos(strtolower($src), 'soundcloud.com') !== false) {
     //$o .= '<iframe id="hyperplayer" data-player-type="soundcloud" width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="'.$src.'&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>';
-    $o .= '<iframe id="hyperplayer'.$id.'" class="hyperaudio-player" data-player-type="soundcloud" width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="'.$src.'"></iframe><script src="https://w.soundcloud.com/player/api.js"></script>';
+    $o .= '<iframe id="hyperplayer'.$id.'" class="hyperaudio-player" data-player-type="soundcloud" width="'.$width.'" height="166" scrolling="no" frameborder="no" allow="autoplay" src="'.$src.'"></iframe><script src="https://w.soundcloud.com/player/api.js"></script>';
   } elseif (strpos(strtolower($src), '.mp3') !== false) {
-    $o .= '<audio id="hyperplayer'.$id.'" class="hyperaudio-player" style="position:relative; width:100%" src="'.$src.'" controls></audio>';
+    $o .= '<audio id="hyperplayer'.$id.'" class="hyperaudio-player" style="position:relative; width:'.$width.'" src="'.$src.'" controls></audio>';
   } else {
-    $o .= '<video id="hyperplayer'.$id.'" class="hyperaudio-player" style="position:relative; width:100%" src="'.$src.'" controls></video>';
+    $o .= '<video id="hyperplayer'.$id.'" class="hyperaudio-player" style="position:relative; width:'.$width.'" src="'.$src.'" controls></video>';
   }
 
 
   //<iframe allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" src="https://www.youtube.com/embed/EAmmUIEsN9A?html5=1&amp;rel=0&amp;modestbranding=1&amp;iv_load_policy=3&amp;disablekb=1&amp;showinfo=0&amp;origin=https%3A%2F%2Fhyperaud.io&amp;controls=0&amp;wmode=opaque&amp;enablejsapi=1&amp;widgetid=1" id="widget2" width="100%" height="100%" frameborder="0"></iframe>
 
- $o .='<div id="hypertranscript'.$id.'" class="hyperaudio-transcript" style="overflow-y:scroll; height:600px; position:relative; border-style:dashed; border-width: 1px; border-color:#999; padding: 8px">'.$transcript.'</div>';
+ $o .='<div id="'.$transcriptid.'" class="hyperaudio-transcript" style="overflow-y:scroll; width:'.$width.'; height:'.$height.'; position:relative; border-style:dashed; border-width: 1px; border-color:#999; padding: 8px">'.$transcript.'</div>';
 
 
   $o .= '<script>
@@ -128,7 +134,7 @@ function hyperaudio_shortcode_handler($atts, $transcript, $tag)
   }).init();
 
   var ht1 = hyperaudiolite();
-  ht1.init("hypertranscript'.$id.'", "hyperplayer'.$id.'", false);
+  ht1.init("'.$transcriptid.'", "hyperplayer'.$id.'", false);
 
   </script>
 ';
